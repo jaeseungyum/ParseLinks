eb6b547789c7e01755b7e9d9b58869261a12e71b
Jaeseung Yum <ym.jsng@gmail.com>
2016-11-29 00:43:28 +0900

    Implement a private function `parser`

---
+++ R/parser.R
@@ -0,0 +1,18 @@
+#' Parse a link header string
+#' then converts link header into a list
+#'
+#' @param link_header_string A length-one character
+#' @return the parsed link header A list
+
+parser <- function(link_header_string) {
+  url_exp <- "(?:<)(.*)(?:>,?\\s?.*)"
+  rel_exp <- "(?:.*rel=[\"|'])(.*)(?:[\"|'])"
+
+  key <- gsub(rel_exp, "\\1", link_header_string)
+  url <- gsub(url_exp, "\\1", link_header_string)
+
+  out <- list()
+  out[[key]] <- list(url = url)
+
+  return(out)
+}
---
+++ tests/testthat/test.parser.R
@@ -0,0 +1,8 @@
+context("parser")
+
+test_that("parser can parse a link header into a meaningful list data", {
+            expect_equal(parser('<alabla>; rel="meta"')
+                         , list(meta = list(url = "alabla")))
+            expect_equal(parser('<alabla2>; rel="prev"')
+                         , list(prev = list(url = "alabla2")))
+})
