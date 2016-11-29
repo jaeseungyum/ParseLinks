5247aa47e2b1c81afd6d631c7b6a71eadeaf0c18
Jaeseung Yum <ym.jsng@gmail.com>
2016-11-30 00:53:59 +0900

    Add package documentation

---
+++ R/ParseLinks.R
@@ -0,0 +1,8 @@
+#' ParseLinks: A simple link header parser for R
+#' 
+#' The ParseLinks package provides a parser function for link header strings. 
+#' By using `parse_links` function, you can get an R list for each contained link in the link header strings.
+#'
+#' @docType package
+#' @name ParseLinks
+NULL 
